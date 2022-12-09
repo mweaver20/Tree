4fd8ac15bd7dc7a42f2b41b3a17c9ee32fb00be4
mweaver20 <83778978+mweaver20@users.noreply.github.com>
2022-12-08 19:26:55 -0800

    Add files via upload

---
+++ Tree/test.js
@@ -0,0 +1,31 @@
+const TreeNode = require('./TreeNode')
+
+const menu = new TreeNode('Menu')
+
+const entries = {
+  Breakfast: ['Cereal', 'BBQ Chicken', 'Oatmeal'],
+  Lunch: ['Soup', 'Sandwich', 'Lasagna'],
+  Dinner: ['Yogurt', 'Filet Mignon', 'Fish Florentine'],
+}
+
+const meals = Object.keys(entries)
+for (let meal = 0; meal < meals.length; meal++) {
+  menu.addChild(meals[meal])
+  const entrylist = entries[meals[meal]]
+  entrylist.forEach((entry) => {
+    menu.children[meal].addChild(entry)
+  })
+}
+
+//removing incorrectly placed menu items
+menu.removeChild('BBQ Chicken')
+menu.removeChild('Yogurt')
+
+//adding items to correct spot on tree
+menu.children[0].addChild('Yogurt')
+menu.children[2].addChild('BBQ Chicken')
+
+console.log('---------- Corrected Menu')
+menu.print()
+
+menu.depthFirstTraversal()
